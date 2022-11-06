893cf28d307e89565ab6936227f0e0023d7b16ad
Darin H. Hajou <it_related@hotmail.com>
2022-11-06 20:30:42 +0100

    Added principal classes

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,14 @@
 <template>
-	
+	<div class="app">
+		<!-- Multiple slides, the slide that have met the conditions is displayed  -->
+		<carousel>
+		<carousel-slide></carousel-slide>
+		<carousel-slide></carousel-slide>
+		<carousel-slide></carousel-slide>
+		<carousel-slide></carousel-slide>
+		<carousel-slide></carousel-slide>
+		</carousel>
+	</div>
 </template>
 
 <script>
--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
-	
+	<div class="carousel"></div>
 </template>
 
 <script>
